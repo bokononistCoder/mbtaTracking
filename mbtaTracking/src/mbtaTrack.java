4ffb338d0165941ac8c5b8c74715e89530fb2f4f
ajacob <ash.is.jacob@gmail.com>
2012-05-07 17:48:11 -0400

    Pull data from server. Parse through XML data

--- mbtaTracking/src/mbtaTrack.java
+++ mbtaTracking/src/mbtaTrack.java
@@ -1,12 +1,26 @@
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
 
 public class mbtaTrack {
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args)  throws IOException{
 		// TODO Auto-generated method stub
-
+		URL url = new URL("http://developer.mbta.com/Data/Red.xml");
+        try
+        {
+        	URLConnection conn = url.openConnection();
+        	readXML rx=new readXML(conn.getInputStream());
+        }
+        catch(Exception e)
+        {
+        	System.out.println("Unable to connect to MBTA");
+        }
+        
 	}
 
 }
